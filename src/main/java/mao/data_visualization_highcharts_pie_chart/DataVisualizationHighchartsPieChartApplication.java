555e3ee5598a3d373f326800f30b137338a7e0ea
mao <1296193245@qq.com>
2023-06-11 16:12:59 +0800

    data_visualization_highcharts_pie_chart

---
+++ src/main/java/mao/data_visualization_highcharts_pie_chart/DataVisualizationHighchartsPieChartApplication.java
@@ -0,0 +1,15 @@
+package mao.data_visualization_highcharts_pie_chart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataVisualizationHighchartsPieChartApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DataVisualizationHighchartsPieChartApplication.class, args);
+    }
+
+}
